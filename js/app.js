b952d234d79271a6f29fdfe9681a386a5d4942f5
Stephen <stephenlevesque2@gmail.com>
2023-02-08 21:58:12 -0700

    lab 3

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 console.log('Hello world');
+let score = 0;
 
 let username = prompt('Hey you! What is your name?');
 alert(`Welcome to my page page ${username}! Guess yes or no on these next questions!`);
@@ -8,44 +9,92 @@ alert(`Welcome to my page page ${username}! Guess yes or no on these next questi
 let questionOneGuess = prompt('Do I play a musical instrument?').toLowerCase();
 
 
-if(questionOneGuess === 'yes' || questionOneGuess === 'y'){
+if (questionOneGuess === 'yes' || questionOneGuess === 'y') {
   alert('You are correct!');
-} else if(questionOneGuess === 'no' || questionOneGuess === 'n'){
+  score++;
+} else if (questionOneGuess === 'no' || questionOneGuess === 'n') {
   alert('Sorry! Wrong!');
 }
 
 let questionTwoGuess = prompt('Do I like dogs more than cats?').toLowerCase();
 
-if(questionTwoGuess === 'yes' || questionTwoGuess === 'y'){
+if (questionTwoGuess === 'yes' || questionTwoGuess === 'y') {
   alert('You are correct!');
-} else if(questionTwoGuess === 'no' || questionTwoGuess === 'n'){
+  score++;
+} else if (questionTwoGuess === 'no' || questionTwoGuess === 'n') {
   alert('Sorry! Wrong!');
 }
 
 let questionThreeGuess = prompt('Do I like cilantro?').toLowerCase();
 
-if(questionThreeGuess === 'no' || questionThreeGuess === 'n'){
+if (questionThreeGuess === 'no' || questionThreeGuess === 'n') {
   alert('You are correct!');
-} else if(questionThreeGuess === 'yes' || questionThreeGuess === 'y'){
+  score++;
+} else if (questionThreeGuess === 'yes' || questionThreeGuess === 'y') {
   alert('Sorry! Wrong!');
 }
 
 let questionFourGuess = prompt('Do I like soccer?').toLowerCase();
 
-if(questionFourGuess === 'yes' || questionFourGuess === 'y'){
+if (questionFourGuess === 'yes' || questionFourGuess === 'y') {
   alert('You are correct!');
-} else if(questionFourGuess === 'no' || questionFourGuess === 'n'){
+  score++;
+} else if (questionFourGuess === 'no' || questionFourGuess === 'n') {
   alert('Sorry! Wrong!');
 }
 
 let questionFiveGuess = prompt('Am I married?').toLowerCase();
 
-if(questionFiveGuess === 'yes' || questionFiveGuess === 'y'){
+if (questionFiveGuess === 'yes' || questionFiveGuess === 'y') {
   alert('You are correct!');
-} else if(questionFiveGuess === 'no' || questionFiveGuess === 'n'){
+  score++;
+} else if (questionFiveGuess === 'no' || questionFiveGuess === 'n') {
   alert('Sorry! Wrong!');
 }
 
-alert(`Thanks for visiting ${username}!`)
+
+
+let myNum = 70;
+let guesses = 4;
+for (let i = 0; i < 4; i++) {
+  // console.log(`You have ${guesses} left!`)
+  let answer = prompt(`${guesses} attempts! What number am I thinking about?`);
+  answer = parseInt(answer);
+  if (answer === myNum) {
+    alert('You are correct!');
+    score++;
+    break;
+  } else {
+    if (answer < myNum) {
+      alert('Too low!');
+      guesses--;
+    } else {
+      alert('Too high!');
+      guesses--;
+    }
+  }
+}
+
+let favFoodGuess = ['ribs', 'pizza', 'spaghetti', 'pasta', 'salad', 'dessert'];
+let attempts = 6;
+
+for (let i = 0; i < 6; i++) {
+  // console.log(`On this iteration I ate: ${favFoodGuess[i]}`);
+  let answer = prompt(`${attempts} tries! What is my favorite food?`);
+  let favFoodPosition = 5;
+
+  while (favFoodPosition >= 0) {
+    if (answer === favFoodGuess[favFoodPosition]) {
+      alert('That is correct!');
+      score++;
+      i = 6;
+      break;
+    } else {
+      favFoodPosition--;
+    }
+  }
+  attempts--;
+}
+alert(`Thanks for visiting ${username}! Your score was ${score}!`);
 
 
